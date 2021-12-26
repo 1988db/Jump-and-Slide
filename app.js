648a341225c708d2b91b81b55d47036b3412d1db
1988db <elozrana@gmail.com>
2021-12-26 23:54:33 +0100

    function jump add

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-    console.log('DOM Content Loaded');
+    let character = document.querySelector('.character');
+    let bottom = 0;
+    
+    function jump() {
+        let timerUpId = setInterval(function() {
+            if (bottom > 250) {
+                clearInterval(timerUpId);
+                let timerDownId = setInterval( function() {
+                    if (bottom < 0) {
+                        clearInterval(timerDownId);
+                    }
+
+                    bottom -= 5;
+                    character.style.bottom = bottom + 'px';
+                }, 20)
+            }
+
+            bottom += 30;
+            character.style.bottom = bottom + 'px';
+        }, 20)
+        
+    }
+    
+    //assign functions to keycodes
+
+    function control(e) {
+        if (e.keyCode === 38) {
+            jump();
+        }
+    }
+    document.addEventListener('keydown', control)
 })
